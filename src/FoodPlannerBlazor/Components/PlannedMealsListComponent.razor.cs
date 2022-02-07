8503ab73fb87b88ff33c4bdd88c60a59ab96dc5f
Michał B <MadTiger2409@users.noreply.github.com>
2022-02-07 12:32:59 +0100

    Fixed .gitignore

---
+++ src/FoodPlannerBlazor/Components/PlannedMealsListComponent.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+
+namespace FoodPlannerBlazor.Components
+{
+    public partial class PlannedMealsListComponent : ComponentBase
+    {
+        [Parameter]
+        public DateTime From { get; set; } = DateTime.UtcNow.Date;
+
+        [Parameter]
+        public DateTime To { get; set; } = DateTime.UtcNow.Date;
+
+        public List<int> Ints { get; set; } = new() { 1, 2, 3 };
+    }
+}
