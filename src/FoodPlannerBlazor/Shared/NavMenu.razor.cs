05949647770033bc807a46bfd8d8800d438c19ca
Michał B <MadTiger2409@users.noreply.github.com>
2022-02-23 13:50:05 +0100

    Little change to the navMenu.

---
+++ src/FoodPlannerBlazor/Shared/NavMenu.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FoodPlannerBlazor.Shared
+{
+    public partial class NavMenu : ComponentBase
+    {
+        private bool collapseNavMenu = true;
+        private bool plannedMealsSubMenuVisible = false;
+
+        private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu() => collapseNavMenu = !collapseNavMenu;
+
+        private void ToggleSubMenu(ref bool subMenuFlag) => subMenuFlag = !subMenuFlag;
+    }
+}
