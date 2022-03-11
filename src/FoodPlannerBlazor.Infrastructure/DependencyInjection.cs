099cfb49766d4b9b3e0ca57a4cc2372e9958c035
Michał B <MadTiger2409@users.noreply.github.com>
2022-03-11 15:32:08 +0100

    changes

--- src/FoodPlannerBlazor.Infrastructure/DependencyInjection.cs
+++ src/FoodPlannerBlazor.Infrastructure/DependencyInjection.cs
@@ -4,41 +4,43 @@ namespace FoodPlannerBlazor.Infrastructure
 {
     public static class DependencyInjection
     {
+        private static readonly string baseApiAddress = "https://localhost:5001";
+
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddHttpClient("categories", client =>
             {
-                client.BaseAddress = new("https://localhost:5001/webapi/categories/");
+                client.BaseAddress = new($"{baseApiAddress}/webapi/categories/");
                 client.DefaultRequestHeaders.Add("Accept", "*/*");
             });
 
             services.AddHttpClient("meals", client =>
             {
-                client.BaseAddress = new("https://localhost:5001/webapi/meals/");
+                client.BaseAddress = new($"{baseApiAddress}/webapi/meals/");
                 client.DefaultRequestHeaders.Add("Accept", "*/*");
             });
 
             services.AddHttpClient("plannedMeals", client =>
             {
-                client.BaseAddress = new("https://localhost:5001/webapi/plannedMeals/");
+                client.BaseAddress = new($"{baseApiAddress}/webapi/plannedMeals/");
                 client.DefaultRequestHeaders.Add("Accept", "*/*");
             });
 
             services.AddHttpClient("products", client =>
             {
-                client.BaseAddress = new("https://localhost:5001/webapi/products/");
+                client.BaseAddress = new($"{baseApiAddress}/webapi/products/");
                 client.DefaultRequestHeaders.Add("Accept", "*/*");
             });
 
             services.AddHttpClient("shoppingList", client =>
             {
-                client.BaseAddress = new("https://localhost:5001/webapi/shoppingList/");
+                client.BaseAddress = new($"{baseApiAddress}/webapi/shoppingList/");
                 client.DefaultRequestHeaders.Add("Accept", "*/*");
             });
 
             services.AddHttpClient("units", client =>
             {
-                client.BaseAddress = new("https://localhost:5001/webapi/units/");
+                client.BaseAddress = new($"{baseApiAddress}/webapi/units/");
                 client.DefaultRequestHeaders.Add("Accept", "*/*");
             });
 
