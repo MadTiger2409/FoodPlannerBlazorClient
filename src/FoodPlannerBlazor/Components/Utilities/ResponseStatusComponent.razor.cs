cb808628a17d3ede1241f6ed99aa4dc1d58c5190
Michał B <MadTiger2409@users.noreply.github.com>
2022-06-07 15:11:57 +0200

    Created ResponseStatusComponent

---
+++ src/FoodPlannerBlazor/Components/Utilities/ResponseStatusComponent.razor.cs
@@ -0,0 +1,33 @@
+﻿using FoodPlannerBlazor.Infrastructure.Common;
+using Microsoft.AspNetCore.Components;
+
+namespace FoodPlannerBlazor.Components.Utilities
+{
+    public partial class ResponseStatusComponent<T> : ComponentBase
+    {
+        private ApiResponse<T> _response;
+        private string _successMessage;
+        private bool _showDetailsInformation = false;
+
+        [Parameter]
+        public ApiResponse<T> Response
+        {
+            get => _response;
+            set => _response = value;
+        }
+
+        [Parameter]
+        public string SuccessMessage
+        {
+            get => _successMessage;
+            set => _successMessage = value;
+        }
+
+        [Parameter]
+        public bool ShowDetailsInformation
+        {
+            get => _showDetailsInformation;
+            set => _showDetailsInformation = value;
+        }
+    }
+}
