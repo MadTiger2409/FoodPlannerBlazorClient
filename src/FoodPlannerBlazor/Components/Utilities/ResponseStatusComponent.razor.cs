7b8548f2d18bdffea0f00cba810fa2f65d706491
Michał B <MadTiger2409@users.noreply.github.com>
2022-06-10 09:57:30 +0200

    moved list with entities names to component and created new styles for it

--- src/FoodPlannerBlazor/Components/Utilities/ResponseStatusComponent.razor.cs
+++ src/FoodPlannerBlazor/Components/Utilities/ResponseStatusComponent.razor.cs
@@ -3,52 +3,52 @@ using Microsoft.AspNetCore.Components;
 
 namespace FoodPlannerBlazor.Components.Utilities
 {
-    public partial class ResponseStatusComponent<T> : ComponentBase
-    {
-        private ApiResponse<T> _response;
-        private string _successMessage;
-        private bool _showDetailsInformation = false;
-        private string detailsCssClass;
+	public partial class ResponseStatusComponent<T> : ComponentBase
+	{
+		private ApiResponse<T> _response;
+		private string _successMessage;
+		private bool _showDetailsInformation = false;
+		private string detailsCssClass;
 
-        [Parameter]
-        public ApiResponse<T> Response
-        {
-            get => _response;
-            set
-            {
-                _response = value;
-                detailsCssClass = Response.Success ? "details-success" : "details-fail";
-            }
-        }
+		[Parameter]
+		public ApiResponse<T> Response
+		{
+			get => _response;
+			set
+			{
+				_response = value;
+				detailsCssClass = Response.Success ? "details-success" : "details-fail";
+			}
+		}
 
-        [Parameter]
-        public string SuccessMessage
-        {
-            get => _successMessage;
-            set => _successMessage = value;
-        }
+		[Parameter]
+		public string SuccessMessage
+		{
+			get => _successMessage;
+			set => _successMessage = value;
+		}
 
-        [Parameter]
-        public bool ShowDetailsInformation
-        {
-            get => _showDetailsInformation;
-            set
-            {
-                if (_showDetailsInformation == value)
-                    return;
+		[Parameter]
+		public bool ShowDetailsInformation
+		{
+			get => _showDetailsInformation;
+			set
+			{
+				if (_showDetailsInformation == value)
+					return;
 
-                _showDetailsInformation = value;
-                ShowDetailsInformationChanged.InvokeAsync(value);
-            }
-        }
+				_showDetailsInformation = value;
+				ShowDetailsInformationChanged.InvokeAsync(value);
+			}
+		}
 
-        [Parameter]
-        public EventCallback<bool> ShowDetailsInformationChanged { get; set; }
+		[Parameter]
+		public EventCallback<bool> ShowDetailsInformationChanged { get; set; }
 
-        private void Hide()
-        {
-            detailsCssClass = string.Empty;
-            ShowDetailsInformation = false;
-        }
-    }
+		private void Hide()
+		{
+			detailsCssClass = string.Empty;
+			ShowDetailsInformation = false;
+		}
+	}
 }
