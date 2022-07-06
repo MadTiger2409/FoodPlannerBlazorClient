63bba19549cbfb96792ea41f438f02a5b4085e8a
Michał B <MadTiger2409@users.noreply.github.com>
2022-07-06 19:50:13 +0200

    Working on shopping list component and logic

--- src/FoodPlannerBlazor.Application/BusinessLogic/ShoppingList/Handlers/GetShoppingListFileContentHandler.cs
+++ src/FoodPlannerBlazor.Application/BusinessLogic/ShoppingList/Handlers/GetShoppingListFileContentHandler.cs
@@ -1,6 +1,11 @@
 ﻿using FoodPlannerBlazor.Application.BusinessLogic.ShoppingList.Queries;
 using FoodPlannerBlazor.Infrastructure.Common;
+using FoodPlannerBlazor.Infrastructure.Extensions;
 using MediatR;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,9 +13,25 @@ namespace FoodPlannerBlazor.Application.BusinessLogic.ShoppingList.Handlers
 {
     public class GetShoppingListFileContentHandler : IRequestHandler<GetShoppingListFileContentQuery, ApiResponse<byte[]>>
     {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public GetShoppingListFileContentHandler(IHttpClientFactory clientFactory) => _clientFactory = clientFactory;
+
         public async Task<ApiResponse<byte[]>> Handle(GetShoppingListFileContentQuery request, CancellationToken cancellationToken)
         {
-            return new ApiResponse<byte[]>();
+            var httpClient = _clientFactory.CreateClient("shoppingList");
+
+            var queryParams = new Dictionary<string, string>
+            {
+                ["from"] = request.GetShoppingListModel.From.Date.ToString("yyyy-MM-dd"),
+                ["to"] = request.GetShoppingListModel.To.Date.ToString("yyyy-MM-dd"),
+                ["peopleCount"] = request.GetShoppingListModel.PeopleCount.ToString(CultureInfo.InvariantCulture)
+            };
+            var partialQuery = QueryHelpers.AddQueryString("pdf", queryParams);
+
+            // Change it to do correct deserialization
+            // Maybe add new extension method to get file with deserialization?
+            return await httpClient.GetWithDeserializationAsync<byte[]>(partialQuery);
         }
     }
 }
--- src/FoodPlannerBlazor.Application/BusinessLogic/ShoppingList/Handlers/GetShoppingListHandler.cs
+++ src/FoodPlannerBlazor.Application/BusinessLogic/ShoppingList/Handlers/GetShoppingListHandler.cs
@@ -1,8 +1,12 @@
 ﻿using FoodPlannerBlazor.Application.BusinessLogic.ShoppingList.Queries;
 using FoodPlannerBlazor.Domain.Entities.ShoppingList;
 using FoodPlannerBlazor.Infrastructure.Common;
+using FoodPlannerBlazor.Infrastructure.Extensions;
 using MediatR;
+using Microsoft.AspNetCore.WebUtilities;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,9 +14,24 @@ namespace FoodPlannerBlazor.Application.BusinessLogic.ShoppingList.Handlers
 {
     public class GetShoppingListHandler : IRequestHandler<GetShoppingListQuery, ApiResponse<List<ShoppingListItem>>>
     {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public GetShoppingListHandler(IHttpClientFactory clientFactory) => _clientFactory = clientFactory;
+
         public async Task<ApiResponse<List<ShoppingListItem>>> Handle(GetShoppingListQuery request, CancellationToken cancellationToken)
         {
-            return new ApiResponse<List<ShoppingListItem>>();
+            var httpClient = _clientFactory.CreateClient("shoppingList");
+
+            var queryParams = new Dictionary<string, string>
+            {
+                ["from"] = request.GetShoppingListModel.From.Date.ToString("yyyy-MM-dd"),
+                ["to"] = request.GetShoppingListModel.To.Date.ToString("yyyy-MM-dd"),
+                ["peopleCount"] = request.GetShoppingListModel.PeopleCount.ToString(CultureInfo.InvariantCulture)
+            };
+            var partialQuery = QueryHelpers.AddQueryString(string.Empty, queryParams);
+
+
+            return await httpClient.GetWithDeserializationAsync<List<ShoppingListItem>>(partialQuery);
         }
     }
 }
