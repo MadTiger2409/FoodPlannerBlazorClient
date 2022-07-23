18c2e05db659eefa34cc57da4780fb5f1ed789f0
Michał B <MadTiger2409@users.noreply.github.com>
2022-07-23 09:58:29 +0200

    Working on the component

---
+++ src/FoodPlannerBlazor/Components/Unit/ReadOrUpdateUnitComponent.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FoodPlannerBlazor.Components.Unit
+{
+    public partial class ReadOrUpdateUnitComponent
+    {
+        [Parameter]
+        public int Id { get; set; }
+    }
+}
