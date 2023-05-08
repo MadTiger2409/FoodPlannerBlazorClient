db8e1b2234b2f412b1f8e37b033808f1058fcf28
Michał B <MadTiger2409@users.noreply.github.com>
2023-05-08 19:35:39 +0200

    working on modal that will be used to confirm entity delete operation

---
+++ src/FoodPlannerBlazor/Components/Modals/DeleteEntityModalComponent.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FoodPlannerBlazor.Components.Modals
+{
+	public partial class DeleteEntityModalComponent : ComponentBase
+	{
+		private bool _deleteEntity = false;
+
+		[Parameter]
+		public string EntityName { get; set; }
+
+		[Parameter]
+		public bool DeleteEntity
+		{
+			get => _deleteEntity;
+			set
+			{
+				if (_deleteEntity == value)
+					return;
+
+				_deleteEntity = value;
+				DeleteEntityChanged.InvokeAsync(value);
+			}
+		}
+
+		[Parameter]
+		public EventCallback<bool> DeleteEntityChanged { get; set; }
+	}
+}
